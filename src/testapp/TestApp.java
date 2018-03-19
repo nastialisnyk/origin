d8eab2b8a0cf6f2ddceea8828b7df710a05899cc
Anastasia Lisnyk <nastia.lisnik@gmail.com>
2018-03-19 21:14:54 +0200

    CommitOne

---
+++ src/testapp/TestApp.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testapp;
+public class TestApp {
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+    // TODO code application logic here
+System.out.println("GIT test application");
+}
+}
