8712f6f69475e560f3334aec112cba5cdd0d7245
Anastasia Lisnyk <nastia.lisnik@gmail.com>
2018-03-27 17:58:20 +0300

    Commit Seven

--- src/testapp/TestApp.java
+++ src/testapp/TestApp.java
@@ -4,15 +4,22 @@
  * and open the template in the editor.
  */
 package testapp;
+import java.util.Scanner;
 public class TestApp {
 /**
 * @param args the command line arguments
 */
 public static void main(String[] args) {
     // TODO code application logic here
-float a=20;
-float b=(float) 12.5;
-System.out.println("a+b="+(a+b));
-System.out.println("GIT test application");
-}
+Scanner in = new Scanner(System.in);
+System.out.print("1st num: ");
+double a = Double.valueOf(in.nextLine());
+System.out.print("2nd num: ");
+double b = Double.valueOf(in.nextLine());
+//System.out.println("a+b="+(a+b));
+//System.out.println("GIT test application");
+SimpleCalc obj = new SimpleCalc ( a,  b);
+System.out.println("a+b="+(obj.getSum()));
+System.out.println("a-b="+(obj.getMinus()));
+} 
 }
